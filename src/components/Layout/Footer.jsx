043486774e17cfd3d358ae4451f32d46507089cb
amiya <amiyasingh56@gmail.com>
2024-09-26 13:58:54 +0530

    commit

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../../main";
 import { Link } from "react-router-dom";
-import {FaGithub, FaLinkedin } from "react-icons/fa";
+import {FaLinkedin } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
@@ -10,9 +10,6 @@ const Footer = () => {
     <footer className={isAuthorized ? "footerShow" : "footerHide"}>
       <div>&copy; All Rights Reserved By AS Company.</div>
       <div>
-        <Link to={"https://github.com/amiya-cyber"} target="_blank">
-          <FaGithub />
-        </Link>
         <Link to={"http://linkedin.com/in/gayatri-kadukar-09978727b"} target="_blank">
           <FaLinkedin />
         </Link>
